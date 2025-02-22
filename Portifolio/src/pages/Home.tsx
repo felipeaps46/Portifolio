e8c2459f5d071931be29753446e02ad126ba2ffa
felipeaps46 <felipeaps0918@gmail.com>
2025-02-22 20:04:36 -0300

    Alterações para deploy

--- Portifolio/src/pages/Home.tsx
+++ Portifolio/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import styles from "../styles/home.module.scss"
 
-import { useRef, useEffect, MutableRefObject } from "react";
+import { useRef, useEffect } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import React from "react";
